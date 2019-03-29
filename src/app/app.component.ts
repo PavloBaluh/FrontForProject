8ed66f5a8d6c234dd965fb4c93b66a0ae1309d78
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-03-29 19:08:38 +0200

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,21 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+// login() {
+  //   this.http.post('http://localhost:8080/login', JSON.stringify({
+  //     username: 'asd',
+  //     password: 'asd'
+  //   }), {observe: 'response'}).subscribe((res) => {
+  //     localStorage.setItem('_token', res.headers.get('Authorization'));
+  //   });
+  // }
+  //
+  // getInfo() {
+  //   const headers = new HttpHeaders({'Authorization': localStorage.getItem('_token')});
+  //   this.http.get('http://localhost:8080/home', {
+  //     headers: headers, responseType: 'text'
+  //   }).subscribe((res) => {
+  //     console.log(res);
+  //   });
+  // }
 }
