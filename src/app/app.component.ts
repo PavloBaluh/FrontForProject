3fb24558564394e0920f89a1def351c75b2f81b0
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-04-10 17:46:45 +0300

    login02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {MainService} from './Services/main.service';
 import {ActivatedRoute} from '@angular/router';
+import {User} from './Models/User';
+import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,20 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  name: string;
+  user: User;
 
   constructor(private servise: MainService, private Activatedroute: ActivatedRoute) {
   }
 
+
   ngOnInit() {
-    this.Activatedroute.queryParams.subscribe((res) => {
-      console.log(res);
-    });
+    console.log(1);
+    let a: string = this.servise.getDecodedAccessToken();
+    console.log(a);
+    if (a !== null) {
+      this.user = <User>a;
+    } else {
+      this.user = new User('Незареєстрований');
+    }
   }
 }
