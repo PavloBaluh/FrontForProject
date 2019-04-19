22e35d0c98d09e91cfc2c0604a8257bdce7244b5
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-04-19 17:13:29 +0300

    profile01-logout

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,11 +13,13 @@ export class RegisterComponent {
   constructor(private  servise: MainService) {
   }
 
+
   getForm(form) {
     this.servise.register(form.value.user, form.value.pass, form.value.email).subscribe((res) => {
       this.header = res;
     });
   }
 }
+
 
 
